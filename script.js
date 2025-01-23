1cc534b90b1486e4d2db68f636907dc5fb159ce2
OwlofProfit <owlofprofit@gmail.com>
2025-01-23 20:26:20 +0000

    update

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
 }
+
+window.onclick = function(event) {
+    const modal = document.getElementById("modal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
