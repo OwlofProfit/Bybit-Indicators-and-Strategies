cccc803167ee4a13e57c5e59a6d579e17ef9bc1d
OwlofProfit <owlofprofit@gmail.com>
2025-01-23 19:31:07 +0000

    update

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-// Funkcia na otvorenie modálneho okna
 function openModal(card) {
     const modal = document.getElementById("modal");
-    const modalImage = document.getElementById("modal-image");
-    const cardImage = card.querySelector("img");
+    const modalImg = document.getElementById("modal-image");
+    const imgSrc = card.querySelector("img").src;
 
+    modalImg.src = imgSrc;
     modal.style.display = "flex";
-    modalImage.src = cardImage.src;
 }
 
-// Funkcia na zatvorenie modálneho okna
 function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
 }
-
-// Skrytie modálneho okna pri načítaní stránky
-document.addEventListener("DOMContentLoaded", () => {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
-});
